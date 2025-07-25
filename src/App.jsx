074ed554d8c639b79257f3c6b28156ca4a991510
abhay963 <abhayyadav96312@gmail.com>
2025-07-26 01:26:18 +0530

    added 3d design using spline

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas } from "./components";
 import useLenisScroll from "./utils/useLenisScroll"; // 🧈
+import Spline from "@splinetool/react-spline"; // 👉 Import Spline
 
 const App = () => {
   useLenisScroll(); // 🔁 activate Lenis globally
 
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
+
+          {/* Hero section with Spline on right */}
+          <div className="flex flex-col md:flex-row items-center justify-between px-6 md:px-16 pt-10">
+            
+            {/* Left side: Hero text/content */}
+            <div className="w-full md:w-1/2 z-10">
+              <Hero />
+            </div>
+
+            {/* Right side: Spline model */}
+            <div className="w-full md:w-1/2 flex justify-center items-center h-[500px]">
+              <div className="w-[700px] h-[700px]">
+                <Spline scene="https://prod.spline.design/kBZmUjfcsCPSEwkx/scene.splinecode" />
+              </div>
+            </div>
+          </div>
         </div>
+
+        {/* Remaining Sections */}
         <About />
         <Experience />
         <Feedbacks />
-        <div className='relative z-0'>
+
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
